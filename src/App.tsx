4650a0dba489c40c34093de54ab051d4cca44995
ohah <bookyoon173@gmail.com>
2022-10-02 00:45:09 +0900

    Toggle 기본

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import 'App.css';
 import Dropdown, {DropdownItem} from 'component/dropdown';
+import Toggle from 'component/toggle';
 
 function App() {
   return (
     <div className="App">
-      <Dropdown value={10}>
-        <DropdownItem value={10}> 10 </DropdownItem>
-        <DropdownItem value={20}> 20 </DropdownItem>
-        <DropdownItem value={30}> 30 </DropdownItem>
-        <DropdownItem value="40"> `40` </DropdownItem>
-        <DropdownItem value="50"> `50` </DropdownItem>
-      </Dropdown>
+      <Toggle active />
     </div>
   );
 }
