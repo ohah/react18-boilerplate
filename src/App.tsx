1dddf06fac3564045386b7aad68e963b29f38b88
ohah <bookyoon173@gmail.com>
2022-10-02 10:23:59 +0900

    modal update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import 'App.css';
-import Pagination from 'component/Pagination';
+import Modal from 'component/Modal';
 import {useState} from 'react';
 
 function App() {
-  const onChange = (value: number) => {
-    console.log(value);
-  };
+  const [isModal, setModal] = useState(false);
   return (
     <div className="App">
-      <Pagination count={10} color="blue" page={5} onChange={onChange} />
+      <button type="button" onClick={() => setModal(true)}>
+        모달
+      </button>
+      <Modal isShow={isModal} close={() => setModal(false)}>
+        <button type="button" onClick={() => setModal(false)}>
+          ㅁㄴㅇㄹ
+        </button>
+      </Modal>
     </div>
   );
 }
---
+++ src/component/Modal.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+import React, {useRef} from 'react';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalBody = styled.div`
+  background: #fff;
+  min-width: 32rem;
+  border-radius: 0.75rem;
+  min-height: 16rem;
+`;
+interface ModalProps {
+  isShow: boolean;
+  close: () => void;
+  children: React.ReactNode;
+}
+
+const Modal = (props: ModalProps) => {
+  const wrapper = useRef<HTMLDivElement>(null);
+  const body = useRef<HTMLDivElement>(null);
+  const onWrapper = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (e.currentTarget === wrapper.current) {
+      props.close();
+    }
+  };
+  const onBody = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  return (
+    <>
+      {props.isShow === true ? (
+        <ModalWrapper onClick={onWrapper} ref={wrapper}>
+          <ModalBody onClick={onBody} ref={body}>
+            {props.children}
+          </ModalBody>
+        </ModalWrapper>
+      ) : (
+        <div />
+      )}
+    </>
+  );
+};
+
+export default Modal;
