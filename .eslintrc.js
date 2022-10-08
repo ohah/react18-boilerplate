07ced3f5eb089eb1489b81945c999399591a67e6
ohah <bookyoon173@gmail.com>
2022-10-09 03:01:57 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,6 +71,7 @@ module.exports = {
     'no-useless-concat': 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
+    'import/no-duplicates': 'off',
     'vars-on-top': 'off',
     'no-use-before-define': 'off',
     'import/prefer-defalut-export': 'off',
