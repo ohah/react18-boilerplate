e99e6894620f09bc54d981e9bb50e7eb40a84b5a
ohah <bookyoon173@gmail.com>
2022-10-26 18:43:31 +0900

    toast animation add

--- src/components/toast/ToastContainer.tsx
+++ src/components/toast/ToastContainer.tsx
@@ -1,5 +1,5 @@
 import { ToastContext, ToastProps } from 'context/ToastContext';
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 
 const ShowAnimation = keyframes`
