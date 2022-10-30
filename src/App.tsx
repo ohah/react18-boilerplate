64726dea5dae8b2dc56636632ec1be818092032b
grtonksj <supksj@gmail.com>
2022-10-30 23:38:00 +0900

    UPDATE: app.tsx 툴팁 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import reactLogo from 'assets/react.svg';
 import React, { useState } from 'react';
 import 'App.css';
+import Tooltip from 'components/Tooltip';
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log(';asdf');
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button type="button" onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
 
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Tooltip title="타이틀" content="콘텐츠">
+        <div> 툴팁 </div>
+      </Tooltip>
     </div>
   );
 }
