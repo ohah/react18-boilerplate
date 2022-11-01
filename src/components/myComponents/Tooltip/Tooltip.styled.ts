492a2b78b69f28a5e0a190e68b83d3b16c7156f1
ub <kimub1204@naver.com>
2022-11-01 23:38:48 +0900

    style: 툴팁 디자인 추가

--- src/components/myComponents/Tooltip/Tooltip.styled.ts
+++ src/components/myComponents/Tooltip/Tooltip.styled.ts
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
 
 export const Message = styled.div`
+  position: absolute;
+  top: 33px;
+  left: 25px;
+  width: 200px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  color: white;
+  border-radius: 4px;
+  cursor: default;
+  background-color: black;
   display: none;
 `;
 
